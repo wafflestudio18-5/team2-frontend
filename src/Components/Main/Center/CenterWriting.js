2668530e52b277b952efb0b78040968869c4a26b
gnarmeister <sb9155118@naver.com>
2020-11-23 01:22:31 +0900

    Fix width of CenterWriting

--- src/Components/Main/Center/CenterWriting.js
+++ src/Components/Main/Center/CenterWriting.js
@@ -1,17 +1,22 @@
+import styled from "styled-components"
 import CenterBigWriting from "./CenterBigWriting"
 import CenterSmallWriting from "./CenterSmallWriting"
 import CenterStartButton from "./CenterStartButton"
 
+const CenterWritingStyle = styled.div`
+  width: 550px;
+`
+
 const CenterWriting = () => {
   return (
-    <div>
+    <CenterWritingStyle>
       <CenterBigWriting>Where good ideas find you</CenterBigWriting>
       <CenterSmallWriting>
         Read and share new perspectives on just about any topic. Everyone’s
         welcome. Learn more.
       </CenterSmallWriting>
       <CenterStartButton>Get Started</CenterStartButton>
-    </div>
+    </CenterWritingStyle>
   )
 }
 
