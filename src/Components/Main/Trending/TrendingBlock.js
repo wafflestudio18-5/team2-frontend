c279ac04f93c16b87298a492a23fe1634e3656ea
gnarmeister <sb9155118@naver.com>
2020-11-23 18:01:45 +0900

    Set basic structure of TrendingBlock

--- src/Components/Main/Trending/TrendingBlock.js
+++ src/Components/Main/Trending/TrendingBlock.js
@@ -4,8 +4,28 @@ const TrendingBlockStyle = styled.div`
   width: 33.33333333%;
 `
 
+const TrendingBlockNumber = styled.span``
+
+const TrendingBlockWriter = styled.p``
+
+const TrendingBlockTitle = styled.p``
+
+const TrendingBlockDateAndTime = styled.p``
+
 const TrendingBlock = ({ index, title, writer, date, time }) => {
-  return <TrendingBlockStyle></TrendingBlockStyle>
+  return (
+    <TrendingBlockStyle>
+      <TrendingBlockNumber>{index}</TrendingBlockNumber>
+      <div>
+        <TrendingBlockWriter>{writer}</TrendingBlockWriter>
+        <TrendingBlockTitle>{title}</TrendingBlockTitle>
+        <TrendingBlockDateAndTime>
+          {date}
+          {time}
+        </TrendingBlockDateAndTime>
+      </div>
+    </TrendingBlockStyle>
+  )
 }
 
 export default TrendingBlock
