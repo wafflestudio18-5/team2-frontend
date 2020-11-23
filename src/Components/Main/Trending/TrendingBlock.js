20e044a78a7b6d26966f2b6dd9a41a730c9d83f4
gnarmeister <sb9155118@naver.com>
2020-11-23 17:55:30 +0900

    Change style of TrendingList, remove summary from data

--- src/Components/Main/Trending/TrendingBlock.js
+++ src/Components/Main/Trending/TrendingBlock.js
@@ -1,8 +1,10 @@
 import styled from "styled-components"
 
-const TrendingBlockStyle = styled.div``
+const TrendingBlockStyle = styled.div`
+  width: 33.33333333%;
+`
 
-const TrendingBlock = ({ index, title, summary, writer, date, time }) => {
+const TrendingBlock = ({ index, title, writer, date, time }) => {
   return <TrendingBlockStyle></TrendingBlockStyle>
 }
 
--- src/Components/Main/Trending/TrendingList.js
+++ src/Components/Main/Trending/TrendingList.js
@@ -1,23 +1,23 @@
 import styled from "styled-components"
 import TrendingBlock from "./TrendingBlock"
 
-const TrendingListStyle = styled.div``
+const TrendingListStyle = styled.div`
+  display: flex;
+  width: 100%;
+`
 
 const TrendingList = ({ trendingPosts }) => {
   return (
     <TrendingListStyle>
       {trendingPosts.map((post, index) => {
         return (
-          <div>
-            <TrendingBlock
-              index={index}
-              title={post.title}
-              summary={post.summary}
-              writer={post.writer}
-              date={post.date}
-              time={post.time}
-            />
-          </div>
+          <TrendingBlock
+            index={index}
+            title={post.title}
+            writer={post.writer}
+            date={post.date}
+            time={post.time}
+          />
         )
       })}
     </TrendingListStyle>
