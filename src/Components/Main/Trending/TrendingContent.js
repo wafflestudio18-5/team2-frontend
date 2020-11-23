c890db60e79f8ea0f0b04a4f2bb9df75c71f1a79
gnarmeister <sb9155118@naver.com>
2020-11-23 17:34:24 +0900

    Change style of TrendingContent, TrendingWrapper

--- src/Components/Main/Trending/TrendingContent.js
+++ src/Components/Main/Trending/TrendingContent.js
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 
-const TrendingContent = styled.div``
+const TrendingContent = styled.div`
+  width: 100%;
+  max-width: 1192px;
+`
 
 export default TrendingContent
--- src/Components/Main/Trending/TrendingWrapper.js
+++ src/Components/Main/Trending/TrendingWrapper.js
@@ -1,5 +1,20 @@
 import styled from "styled-components"
 
-const TrendingWrapper = styled.div``
+const TrendingWrapper = styled.div`
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+
+  @media (min-width: 1080px) {
+    padding: 0 64px;
+  }
+  @media (max-width: 1079.98px) and (min-width: 728px) {
+    padding: 0 48px;
+  }
+  @media (max-width: 727.98px) {
+    padding: 0 24px;
+  }
+`
 
 export default TrendingWrapper
