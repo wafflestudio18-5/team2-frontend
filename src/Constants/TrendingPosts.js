043d787a775aad1d25abe64b8831825a73b4c125
gnarmeister <sb9155118@naver.com>
2020-11-24 17:37:34 +0900

    Add trending posts

--- src/Constants/TrendingPosts.js
+++ src/Constants/TrendingPosts.js
@@ -1,39 +1,43 @@
 const TrendingPosts = [
   {
-    title: "title",
-    writer: "writer",
-    date: "date",
+    title: "I created my own YouTube algorithm (to stop me wasting time)",
+    writer: "Chris Lovejoy",
+    date: "Nov23",
+    time: "9 min",
+    profileurl:
+      "https://c.files.bbci.co.uk/41CF/production/_109474861_angrycat-index-getty3-3.jpg",
+  },
+  {
+    title: "PC Life After Apple Silicon",
+    writer: "Jean-Louis Gassée in Monday Note",
+    date: "Nov23",
+    time: "5 min",
+    profileurl: "",
+  },
+  {
+    title: "Glassmorphism in user interfaces",
+    writer: "Michal Malewicz in UX Collective",
+    date: "Nov23",
     time: "time",
   },
   {
-    title: "title",
-    writer: "writer",
-    date: "date",
-    time: "time",
+    title: "Are we just not gonna talk about Hassan Minhaj?",
+    writer: "Sangeetha",
+    date: "Nov23",
+    time: "7 min",
   },
   {
-    title: "title",
-    writer: "writer",
-    date: "date",
-    time: "time",
+    title:
+      "Why a Paid Newsletter Won’t Be Enough Money for Most Writers (And That’s Fine): The Multi-SKU Creator",
+    writer: "Hunter Walk",
+    date: "Nov23",
+    time: "3 min",
   },
   {
-    title: "title",
-    writer: "writer",
-    date: "date",
-    time: "time",
-  },
-  {
-    title: "title",
-    writer: "writer",
-    date: "date",
-    time: "time",
-  },
-  {
-    title: "title",
-    writer: "writer",
-    date: "date",
-    time: "time",
+    title: "Mask Confusion Settled: Here’s How Effective They Are",
+    writer: "Robert Roy Britt in Medium Coronavirus Blog",
+    date: "Nov23",
+    time: "4 min",
   },
 ]
 
