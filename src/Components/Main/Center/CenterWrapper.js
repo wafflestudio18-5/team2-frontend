6df0e1270ad2f21b0d76a5860f5b9deac0c2b83f
gnarmeister <sb9155118@naver.com>
2020-11-24 18:47:06 +0900

    Fix CenterWrapper background image

--- src/Components/Main/Center/CenterWrapper.js
+++ src/Components/Main/Center/CenterWrapper.js
@@ -24,14 +24,15 @@ const CenterWrapper = styled.div`
     height: 368px;
   }
 
-  background-origin: border-box;
   background-image: url("https://miro.medium.com/max/770/1*qcAZgT4Sk37MPSTGBH2KUw.png");
   background-size: 385px 341px;
   background-repeat: no-repeat;
   background-position: bottom 59px right 32px;
+  background-origin: content-box;
 
   @media (max-width: 1079.98px) {
     background-position: bottom 59px right 80px;
+    background-origin: padding-box;
   }
 
   @media (max-width: 903.98px) {
