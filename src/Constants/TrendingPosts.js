c17fcfa99ee9d9f2a2e98064b7a5f8cb09533aa1
gnarmeister <sb9155118@naver.com>
2020-11-24 18:36:03 +0900

    Add sample profile images to trending posts

--- src/Constants/TrendingPosts.js
+++ src/Constants/TrendingPosts.js
@@ -12,19 +12,24 @@ const TrendingPosts = [
     writer: "Jean-Louis Gassée in Monday Note",
     date: "Nov23",
     time: "5 min",
-    profileurl: "",
+    profileurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQo4BQSpilYy5KuAptMxbOAxm4uKjFYDG6_wg&usqp=CAU",
   },
   {
     title: "Glassmorphism in user interfaces",
     writer: "Michal Malewicz in UX Collective",
     date: "Nov23",
     time: "time",
+    profileurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlNyI5Bbsl1vq1BQjH9XA-Z4j0Kkk0cEpAnA&usqp=CAU",
   },
   {
     title: "Are we just not gonna talk about Hassan Minhaj?",
     writer: "Sangeetha",
     date: "Nov23",
     time: "7 min",
+    profileurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSc35_l0ccvyNykQDrZpSYG8tMZ0GMMgOcNQ&usqp=CAU",
   },
   {
     title:
@@ -32,12 +37,16 @@ const TrendingPosts = [
     writer: "Hunter Walk",
     date: "Nov23",
     time: "3 min",
+    profileurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2pEEL0hC_J2HGb9gRSI67QGYlLZM6RlOR2w&usqp=CAU",
   },
   {
     title: "Mask Confusion Settled: Here’s How Effective They Are",
     writer: "Robert Roy Britt in Medium Coronavirus Blog",
     date: "Nov23",
     time: "4 min",
+    profileurl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmFbie3AFw3Q0mjEUZpY8HjbW6005Fx45CnQ&usqp=CAU",
   },
 ]
 
