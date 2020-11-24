b846a145cb190bca81474950ca7fd2abc29a0fa0
gnarmeister <sb9155118@naver.com>
2020-11-24 18:24:47 +0900

    Fix responsive of Center background image

--- src/Components/Main/Center/CenterContents.js
+++ src/Components/Main/Center/CenterContents.js
@@ -4,10 +4,6 @@ const CenterContents = styled.div`
   max-width: 1192px;
   width: 100%;
   height: 100%;
-  background-image: url("https://miro.medium.com/max/770/1*qcAZgT4Sk37MPSTGBH2KUw.png");
-  background-size: 385px 341px;
-  background-repeat: no-repeat;
-  background-position: bottom 59px right 32px;
 `
 
 export default CenterContents
--- src/Components/Main/Center/CenterWrapper.js
+++ src/Components/Main/Center/CenterWrapper.js
@@ -23,6 +23,28 @@ const CenterWrapper = styled.div`
     padding: 0 24px;
     height: 368px;
   }
+
+  background-origin: border-box;
+  background-image: url("https://miro.medium.com/max/770/1*qcAZgT4Sk37MPSTGBH2KUw.png");
+  background-size: 385px 341px;
+  background-repeat: no-repeat;
+  background-position: bottom 59px right 32px;
+
+  @media (max-width: 1079.98px) {
+    background-position: bottom 59px right 80px;
+  }
+
+  @media (max-width: 903.98px) {
+    background-position: bottom 59px right 48px;
+  }
+
+  @media (max-width: 727.98px) {
+    background-position: bottom 28px right -64px;
+  }
+
+  @media (max-width: 551.98px) {
+    background-position: bottom 28px right -126px;
+  }
 `
 
 export default CenterWrapper
