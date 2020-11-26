18e6eb897dc75288fd09ceb381235b0d66495e61
gnarmeister <sb9155118@naver.com>
2020-11-26 18:59:23 +0900

    Fix style of buttons in signup modal

--- src/Components/AuthModal/ModalButton.js
+++ src/Components/AuthModal/ModalButton.js
@@ -10,6 +10,15 @@ const ModalButton = styled.button`
   border: 1px solid ${Colors.borderGray};
   border-radius: 4px;
   color: ${Colors.borderBlack};
+  line-height: 20px;
+  text-align: left;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  :hover {
+    cursor: pointer;
+  }
 `
 
 export default ModalButton
