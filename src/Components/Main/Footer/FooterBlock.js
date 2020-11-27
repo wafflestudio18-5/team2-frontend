4734428b37315cf5fefe3a7b27617a6469b75b2b
jeongdaun <daun20211@naver.com>
2020-11-28 01:51:53 +0900

    Finished Footer

---
+++ src/Components/Main/Footer/FooterBlock.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+import Color from "../../../Constants/Color"
+
+const FooterBlock = styled.a`
+    font-size: 14px;
+    font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    color: ${Color.gray};
+    line-height: 20px;
+    font-weight: 400;
+    margin: 0;
+    text-decoration: none;
+    margin-right: 24px;
+    margin-bottom: 8px;
+`
+export default FooterBlock
--- src/Components/Main/Footer/FooterWrapper.js
+++ src/Components/Main/Footer/FooterWrapper.js
@@ -3,5 +3,8 @@ import styled from 'styled-components'
 const FooterWrapper = styled.div`
     width: 100%;
     display: flex;
+    padding: 24px 0;
+    flex-wrap: wrap;
+    flex-direction: row;
 `
 export default FooterWrapper
