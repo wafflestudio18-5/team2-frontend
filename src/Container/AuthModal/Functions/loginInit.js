4bad8a752987029a3af63534621c59f26e855b05
gnarmeister <sb9155118@naver.com>
2020-12-27 17:21:32 +0900

    Catch error in logininit, signupinit functions

--- src/Container/AuthModal/Functions/loginInit.js
+++ src/Container/AuthModal/Functions/loginInit.js
@@ -3,16 +3,15 @@ import ModalTypeConstants from "../../../Constants/ModalTypeConstants"
 
 const loginInit = async (email, setModalType, setEmail) => {
   try {
-    console.log(
-      await postUserLogin({
-        auth_type: "EMAIL",
-        req_type: "INIT",
-        email,
-      })
-    )
+    await postUserLogin({
+      auth_type: "EMAIL",
+      req_type: "INIT",
+      email,
+    })
     setModalType(ModalTypeConstants.EMAIL_SENT)
   } catch (error) {
     setEmail("")
+    document.getElementById("EmailInputField").value = ""
     setModalType(ModalTypeConstants.EMAIL_NOT_EXISTS)
   }
 }
