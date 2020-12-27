5282fc02bc30a30b554581c72f0a891c7c1dbc67
gnarmeister <sb9155118@naver.com>
2020-12-27 22:09:23 +0900

    Add callback component and add query-string package to project

---
+++ src/Components/Callback/index.js
@@ -0,0 +1,10 @@
+const Callback = ({ token, operation }) => {
+  return (
+    <div>
+      {token}&nbsp;
+      {operation}
+    </div>
+  )
+}
+
+export default Callback
--- src/Pages/Callback/Callback.js
+++ src/Pages/Callback/Callback.js
@@ -1,5 +1,11 @@
+import Callback from "../../Components/Callback"
+import { useLocation } from "react-router-dom"
+import queryString from "query-string"
+
 const CallbackPage = () => {
-  return <div>hi</div>
+  const queryStrings = queryString.parse(useLocation().search)
+  console.log(queryStrings)
+  return <Callback {...queryStrings} />
 }
 
 export default CallbackPage
