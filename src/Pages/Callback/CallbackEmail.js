0a65e06686c6fc72d458fff0fd644899e21325d1
gnarmeister <sb9155118@naver.com>
2020-12-28 23:49:04 +0900

    Add useEffect for getting data from server

--- src/Pages/Callback/CallbackEmail.js
+++ src/Pages/Callback/CallbackEmail.js
@@ -3,7 +3,7 @@ import signUpCheck from "./Functions/signUpCheck"
 import login from "./Functions/login"
 import { useLocation, useHistory } from "react-router-dom"
 import queryString from "query-string"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useCookies } from "react-cookie"
 import TokenStatus from "../../Constants/TokenStatus"
 
@@ -12,22 +12,27 @@ const CallbackEmailPage = () => {
   // operation=register인 경우 email을 받아 페이지에 출력하기 위한 state
   const [tokenStatus, setTokenStatus] = useState(TokenStatus.NOT_EXIST)
   // 토큰의 상태(유효한지 아닌지)
+  const [input, setInput] = useState("")
+  // input field의 값
   const queryStrings = queryString.parse(useLocation().search)
   const history = useHistory()
   const setCookie = useCookies(["auth"])[1]
 
-  switch (queryStrings.operation) {
-    case "register":
-      signUpCheck(queryStrings.token, history, setEmail, setTokenStatus)
-      break
+  useEffect(() => {
+    switch (queryStrings.operation) {
+      case "register":
+        signUpCheck(queryStrings.token, history, setEmail, setTokenStatus)
+        break
 
-    case "login":
-      login(queryStrings.token, history, setCookie, setTokenStatus)
-      break
+      case "login":
+        login(queryStrings.token, history, setCookie, setTokenStatus)
+        break
 
-    default:
-      break
-  }
+      default:
+        break
+    }
+  }, [queryStrings.operation, queryStrings.token, history, setCookie])
+
   return (
     <CallbackEmail
       email={email}
