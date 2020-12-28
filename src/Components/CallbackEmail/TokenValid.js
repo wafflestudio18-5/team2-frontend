f559a1a313b43bb83171ed07aeded0edce5f3783
gnarmeister <sb9155118@naver.com>
2020-12-28 17:54:31 +0900

    Style callback page when token is valid

---
+++ src/Components/CallbackEmail/TokenValid.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+import BigWriting from "./BigWriting"
+import MediumWriting from "./MediumWriting"
+import SmallWriting from "./SmallWriting"
+import Input from "./Input"
+import Button from "./Button"
+
+const DivStyle = styled.div`
+  text-align: center;
+  padding: 100px 60px;
+`
+
+const TokenValid = ({ email, token }) => {
+  return (
+    <DivStyle>
+      <BigWriting>Almost there!</BigWriting>
+      <MediumWriting>
+        Finish creating your account for the full Medium experience.
+      </MediumWriting>
+      <SmallWriting>Your full name</SmallWriting>
+      <Input />
+      <SmallWriting>Your email</SmallWriting>
+      <MediumWriting>{email}</MediumWriting>
+      <Button>Create account</Button>
+    </DivStyle>
+  )
+}
+
+export default TokenValid
--- src/Components/CallbackEmail/index.js
+++ src/Components/CallbackEmail/index.js
@@ -2,16 +2,23 @@ import Header from "./Header"
 import TokenInvalid from "./TokenInvalid"
 import TokenValid from "./TokenValid"
 import TokenStatus from "../../Constants/TokenStatus"
+import styled from "styled-components"
+
+const DivStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 const CallbackEmail = ({ email, tokenStatus, token }) => {
   return (
-    <div>
+    <DivStyle>
       <Header />
       {tokenStatus === TokenStatus.VALID && (
         <TokenValid email={email} token={token} />
       )}
       {tokenStatus === TokenStatus.INVALID && <TokenInvalid />}
-    </div>
+    </DivStyle>
   )
 }
 
