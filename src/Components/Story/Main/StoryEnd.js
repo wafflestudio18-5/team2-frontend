0abfb46fab7ac79234ac94a920f54a2346f8e34d
jeongdaun <daun20211@naver.com>
2020-12-29 15:59:51 +0900

    Finished StoryEnd

--- src/Components/Story/Main/StoryEnd.js
+++ src/Components/Story/Main/StoryEnd.js
@@ -2,16 +2,208 @@ import styled from 'styled-components';
 import Color from '../../../Constants/Color';
 
 const StoryEndWrapper = styled.div`
+    display: block;
     margin: 0 auto;
     max-width: 680px;
     min-width: 0px;
     width: 100%;
-    background: red;
-    justify-content: center;
+`;
+const UserEndWrapper = styled.div`
+    display: block;
+    margin-top: 25px;
+    padding-bottom: 25px;
 `;
 
+const UserEndBlock = styled.div`
+    display: flex;
+    margin-top: 25px;
+    justify-content: space-between;
+`;
+
+const EndLeftBlock = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const ClapBlock = styled.div`
+    max-width: 155px;
+    display: block;
+    background: 0;
+    border: 0;
+`;
+
+const ClapButton = styled.button`
+    cursor: pointer;
+    border: 0;
+    outline: 0;
+    padding: 0;
+    background: 0;
+    fill: ${Color.borderBlack};
+    &:hover {
+        fill: ${Color.gray};
+    }
+`;
+
+const Blank = styled.div`
+    display: block;
+    width: 25px;
+`;
+
+const ResponseButton = styled.button`
+    cursor: pointer;
+    border: 0;
+    padding-right: 6px;
+    margin: 0;
+    background: 0;
+    fill: ${Color.borderBlack};
+`;
+
+const EndRightBlock = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const ShareButton = styled.button`
+    cursor: pointer;
+    border: 0;
+    background: 0;
+    fill: ${Color.borderBlack};
+    margin: 0;
+    padding-right: 6px;
+`;
+
+const BookmarkButton = styled.button`
+    cursor: pointer;
+    border: 0;
+    background: 0;
+    fill: ${Color.borderBlack};
+    margin: 0;
+    padding-right: 8px;
+`;
+
+const EtcButton = styled.button`
+    cursor: pointer;
+    border: 0;
+    background: 0;
+    fill: ${Color.borderBlack};
+    margin: 0;
+    padding-right: 8px;
+`;
+
+const TagWrapper = styled.div`
+    padding-bottom: 48px;
+    margin-top: 25px;
+    display: block;
+`
+
+const TagBlock = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    list-style-image: none;
+`
+
+const TagStyle = styled.li`
+    display: inline-block;
+    margin-bottom: 8px;
+    list-style-type: none;
+    margin-right: 8px;
+`
+
+const Tag = styled.a`
+    display: block;
+    padding: 5px 10px;
+    border-radius: 3px;
+    line-height: 22px;
+    font-size: 13px;
+    background: ${Color.backgroundGray};
+    text-decoration: none;
+    color: ${Color.gray};
+    font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-weight: 400;
+`
+
 const StoryEnd = () => {
-    return <StoryEndWrapper>StoryEnd</StoryEndWrapper>;
+    return (
+        <StoryEndWrapper>
+            <UserEndWrapper>
+                <UserEndBlock>
+                    <EndLeftBlock>
+                        <ClapBlock>
+                            <ClapButton>
+                                <svg width="33" height="33" viewBox="0 0 33 33" aria-label="clap">
+                                    <path
+                                        d="M28.86 17.34l-3.64-6.4c-.3-.43-.71-.73-1.16-.8a1.12 1.12 0 0 0-.9.21c-.62.5-.73 1.18-.32 2.06l1.22 2.6 1.4 2.45c2.23 4.09 1.51 8-2.15 11.66a9.6 9.6 0 0 1-.8.71 6.53 6.53 0 0 0 4.3-2.1c3.82-3.82 3.57-7.87 2.05-10.39zm-6.25 11.08c3.35-3.35 4-6.78 1.98-10.47L21.2 12c-.3-.43-.71-.72-1.16-.8a1.12 1.12 0 0 0-.9.22c-.62.49-.74 1.18-.32 2.06l1.72 3.63a.5.5 0 0 1-.81.57l-8.91-8.9a1.33 1.33 0 0 0-1.89 1.88l5.3 5.3a.5.5 0 0 1-.71.7l-5.3-5.3-1.49-1.49c-.5-.5-1.38-.5-1.88 0a1.34 1.34 0 0 0 0 1.89l1.49 1.5 5.3 5.28a.5.5 0 0 1-.36.86.5.5 0 0 1-.36-.15l-5.29-5.29a1.34 1.34 0 0 0-1.88 0 1.34 1.34 0 0 0 0 1.89l2.23 2.23L9.3 21.4a.5.5 0 0 1-.36.85.5.5 0 0 1-.35-.14l-3.32-3.33a1.33 1.33 0 0 0-1.89 0 1.32 1.32 0 0 0-.39.95c0 .35.14.69.4.94l6.39 6.4c3.53 3.53 8.86 5.3 12.82 1.35zM12.73 9.26l5.68 5.68-.49-1.04c-.52-1.1-.43-2.13.22-2.89l-3.3-3.3a1.34 1.34 0 0 0-1.88 0 1.33 1.33 0 0 0-.4.94c0 .22.07.42.17.61zm14.79 19.18a7.46 7.46 0 0 1-6.41 2.31 7.92 7.92 0 0 1-3.67.9c-3.05 0-6.12-1.63-8.36-3.88l-6.4-6.4A2.31 2.31 0 0 1 2 19.72a2.33 2.33 0 0 1 1.92-2.3l-.87-.87a2.34 2.34 0 0 1 0-3.3 2.33 2.33 0 0 1 1.24-.64l-.14-.14a2.34 2.34 0 0 1 0-3.3 2.39 2.39 0 0 1 3.3 0l.14.14a2.33 2.33 0 0 1 3.95-1.24l.09.09c.09-.42.29-.83.62-1.16a2.34 2.34 0 0 1 3.3 0l3.38 3.39a2.17 2.17 0 0 1 1.27-.17c.54.08 1.03.35 1.45.76.1-.55.41-1.03.9-1.42a2.12 2.12 0 0 1 1.67-.4 2.8 2.8 0 0 1 1.85 1.25l3.65 6.43c1.7 2.83 2.03 7.37-2.2 11.6zM13.22.48l-1.92.89 2.37 2.83-.45-3.72zm8.48.88L19.78.5l-.44 3.7 2.36-2.84zM16.5 3.3L15.48 0h2.04L16.5 3.3z"
+                                        fill-rule="evenodd"
+                                    ></path>
+                                </svg>
+                            </ClapButton>
+                        </ClapBlock>
+                        <Blank />
+                        <ResponseButton>
+                            <svg width="33" height="33" viewBox="0 0 33 33" class="r" aria-label="responses">
+                                <path
+                                    clip-rule="evenodd"
+                                    d="M24.28 25.5l.32-.29c2.11-1.94 3.4-4.61 3.4-7.56C28 11.83 22.92 7 16.5 7S5 11.83 5 17.65s5.08 10.66 11.5 10.66c1.22 0 2.4-.18 3.5-.5l.5-.15.41.33a8.86 8.86 0 0 0 4.68 2.1 7.34 7.34 0 0 1-1.3-4.15v-.43zm1 .45c0 1.5.46 2.62 1.69 4.44.22.32.01.75-.38.75a9.69 9.69 0 0 1-6.31-2.37c-1.2.35-2.46.54-3.78.54C9.6 29.3 4 24.09 4 17.65 4 11.22 9.6 6 16.5 6S29 11.22 29 17.65c0 3.25-1.42 6.18-3.72 8.3z"
+                                ></path>
+                            </svg>
+                        </ResponseButton>
+                    </EndLeftBlock>
+                    <EndRightBlock>
+                        <ShareButton>
+                            <svg width="25" height="25" class="r">
+                                <g fill-rule="evenodd">
+                                    <path d="M15.6 5a.42.42 0 0 0 .17-.3.42.42 0 0 0-.12-.33l-2.8-2.79a.5.5 0 0 0-.7 0l-2.8 2.8a.4.4 0 0 0-.1.32c0 .12.07.23.16.3h.02a.45.45 0 0 0 .57-.04l2-2V10c0 .28.23.5.5.5s.5-.22.5-.5V2.93l2.02 2.02c.08.07.18.12.3.13.11.01.21-.02.3-.08v.01"></path>
+                                    <path d="M18 7h-1.5a.5.5 0 0 0 0 1h1.6c.5 0 .9.4.9.9v10.2c0 .5-.4.9-.9.9H6.9a.9.9 0 0 1-.9-.9V8.9c0-.5.4-.9.9-.9h1.6a.5.5 0 0 0 .35-.15A.5.5 0 0 0 9 7.5a.5.5 0 0 0-.15-.35A.5.5 0 0 0 8.5 7H7a2 2 0 0 0-2 2v10c0 1.1.9 2 2 2h11a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2"></path>
+                                </g>
+                            </svg>
+                        </ShareButton>
+                        <BookmarkButton>
+                            <svg width="25" height="25" viewBox="0 0 25 25">
+                                <path
+                                    d="M19 6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v14.66h.01c.01.1.05.2.12.28a.5.5 0 0 0 .7.03l5.67-4.12 5.66 4.13a.5.5 0 0 0 .71-.03.5.5 0 0 0 .12-.29H19V6zm-6.84 9.97L7 19.64V6a1 1 0 0 1 1-1h9a1 1 0 0 1 1 1v13.64l-5.16-3.67a.49.49 0 0 0-.68 0z"
+                                    fill-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </BookmarkButton>
+                        <EtcButton>
+                            <svg class="r ef ia" width="25" height="25">
+                                <path
+                                    d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z"
+                                    fill-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </EtcButton>
+                    </EndRightBlock>
+                </UserEndBlock>
+            </UserEndWrapper>
+            <TagWrapper>
+                <TagBlock>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/tag">tag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/longtag">longtag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/longlongtag">longlongtag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/longlongtag">longlongtag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/verylongtag">very very very very very very very very very very long tag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/tag">tag</Tag>
+                    </TagStyle>
+                    <TagStyle>
+                        <Tag href="https://medium.com/tag/tag">tag</Tag>
+                    </TagStyle>
+                </TagBlock>
+            </TagWrapper>
+        </StoryEndWrapper>
+    );
 };
 
 export default StoryEnd;
