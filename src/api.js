c536cb99f1a283e418c148fe78c7b75a47b5b34b
gnarmeister <sb9155118@naver.com>
2020-12-29 13:43:27 +0900

    Change axios baseurl

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseUrl = "http://localhost:8000/"
+const baseUrl = "https://api.wadium.shop/"
 axios.defaults.baseURL = baseUrl
 
 export const postUser = async (body) => {
