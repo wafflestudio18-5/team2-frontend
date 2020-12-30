95479e00766f17c499ec95b623e277aa2d3b7f7f
gnarmeister <sb9155118@naver.com>
2020-12-30 16:30:49 +0900

    Add comments on api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,18 @@
 import axios from "axios"
 
+// set base url
 const baseUrl = "http://localhost:8000"
 axios.defaults.baseURL = baseUrl
 
+// user api
 export const postUser = async (body) => {
+  // POST /user/
   const response = await axios.post("user/", body)
   return response
 }
 
 export const postUserLogin = async (body) => {
+  // POST /user/login/
   const response = await axios.post("user/login/", body)
   return response
 }
