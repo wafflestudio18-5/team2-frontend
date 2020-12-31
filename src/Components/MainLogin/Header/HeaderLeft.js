764a17304eee394d82fdc711e8f724bc3acff883
gnarmeister <sb9155118@naver.com>
2020-12-31 16:36:01 +0900

    Fix style

--- src/Components/MainLogin/Header/HeaderLeft.js
+++ src/Components/MainLogin/Header/HeaderLeft.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 const HeaderLeft = styled.div`
   display: flex;
+  align-items: center;
 `
 
 export default HeaderLeft
--- src/Components/MainLogin/Header/HeaderRight.js
+++ src/Components/MainLogin/Header/HeaderRight.js
@@ -2,6 +2,7 @@ import styled from "styled-components"
 
 const HeaderRight = styled.div`
   display: flex;
+  align-items: center;
 `
 
 export default HeaderRight
