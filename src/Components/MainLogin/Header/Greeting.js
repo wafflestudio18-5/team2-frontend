7a02a95a819eb593daab8c3b58bd51109a840b18
gnarmeister <sb9155118@naver.com>
2020-12-31 17:51:46 +0900

    Fix style of greeting

--- src/Components/MainLogin/Header/Greeting.js
+++ src/Components/MainLogin/Header/Greeting.js
@@ -1,5 +1,42 @@
+import styled from "styled-components"
+import Color from "../../../Constants/Color"
+
+const GreetingStyle = styled.div`
+  margin-left: 16px;
+  padding-left: 16px;
+  border-left: 1px solid rgba(230, 230, 230, 1);
+  font-weight: 600;
+  color: ${Color.borderBlack};
+  line-height: 28px;
+  font-size: 20px;
+  font-family: "Noto Sans";
+  letter-spacing: -0.03em;
+  white-space: nowrap;
+`
+
 const Greeting = () => {
-  return <div>good afternoon</div>
+  const hour = new Date().getHours()
+
+  let message = ""
+  switch (true) {
+    case 5 <= hour && hour < 12:
+      message = "Good morning"
+      break
+
+    case 12 <= hour && hour < 17:
+      message = "Good afternoon"
+      break
+
+    case 17 <= hour && hour < 24:
+      message = "Good evening"
+      break
+
+    default:
+      message = "Good night"
+      break
+  }
+
+  return <GreetingStyle>{message}</GreetingStyle>
 }
 
 export default Greeting
