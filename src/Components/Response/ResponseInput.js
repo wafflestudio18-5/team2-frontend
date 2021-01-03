a46cb5d35376b9a3fb3da19e3d42667adfe4133b
jeongdaun <daun20211@naver.com>
2021-01-03 20:59:36 +0900

    Added response input window

--- src/Components/Response/ResponseInput.js
+++ src/Components/Response/ResponseInput.js
@@ -3,10 +3,9 @@ import Color from '../../Constants/Color';
 
 const ResponseInputWrapper = styled.div`
     margin-bottom: 20px;
-    padding-top: 20px;
-    padding: 0 24px;
+    padding: 20px 24px 0;
     display: block;
-`
+`;
 
 const ResponseInputBlock = styled.div`
     padding-bottom: 0px;
@@ -16,14 +15,48 @@ const ResponseInputBlock = styled.div`
     flex-direction: column;
     background-color: white;
     display: flex;
+`;
+
+const ResponseInputInnerBlock = styled.div`
+    display: block;
+    min-height: 15px;
+    padding: 14px;
+`;
+
+const ResponseInputWindow = styled.div`
+    outline: none;
+    white-space: pre-wrap;
+    overflow-wrap: break-word;
+`
+
+const ResponseInputText = styled.p`
+    cursor: text;
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    margin: 0;
+    font-family: medium-content-sans-serif-font, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    color: rgba(0,0,0,0.8);
+    opacity: 0.333;
+    user-select: none;
+    font-style: normal;
+    font-weight: normal;
 `
 
 const ResponseInput = () => {
-    return(
+    return (
         <ResponseInputWrapper>
-            <ResponseInputBlock>Response</ResponseInputBlock>
+            <ResponseInputBlock>
+                <ResponseInputInnerBlock>
+                    <ResponseInputWindow data-gramm="false" role="textbox" data-slate-editor="true" data-slate-node="value" contenteditable="true">
+                        <ResponseInputText>What are your thoughts?</ResponseInputText>
+                    </ResponseInputWindow>
+                </ResponseInputInnerBlock>
+            </ResponseInputBlock>
         </ResponseInputWrapper>
     );
-}
+};
 
 export default ResponseInput;
