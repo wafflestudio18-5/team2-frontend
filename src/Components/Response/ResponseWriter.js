0dd08706d4bf96b936beb40c97914dc198fbd514
jeongdaun <daun20211@naver.com>
2021-01-04 00:13:22 +0900

    Changed Dot button to close button

--- src/Components/Response/ResponseWriter.js
+++ src/Components/Response/ResponseWriter.js
@@ -50,7 +50,7 @@ const ResponseTime = styled.h4`
     margin: 0;
 `;
 
-const DotButton = styled.button`
+const CloseButton = styled.button`
     cursor: pointer;
     border: 0;
     background: 0;
@@ -74,14 +74,11 @@ const ResponseWriter = ({ Response }) => {
                     </WriterNameBlock>
                 </WriterInformationBlock>
             </ResponeWriterInformationBlock>
-            <DotButton>
-                <svg class="overflow-dots-filled-25px_svg__svgIcon-use" width="25" height="25">
-                    <path
-                        d="M5 12.5c0 .55.2 1.02.59 1.41.39.4.86.59 1.41.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41A1.93 1.93 0 0 0 7 10.5c-.55 0-1.02.2-1.41.59-.4.39-.59.86-.59 1.41zm5.62 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.42.59.55 0 1.02-.2 1.41-.59.4-.39.59-.86.59-1.41 0-.55-.2-1.02-.59-1.41a1.93 1.93 0 0 0-1.41-.59c-.55 0-1.03.2-1.42.59-.39.39-.58.86-.58 1.41zm5.6 0c0 .55.2 1.02.58 1.41.4.4.87.59 1.43.59.56 0 1.03-.2 1.42-.59.39-.39.58-.86.58-1.41 0-.55-.2-1.02-.58-1.41a1.93 1.93 0 0 0-1.42-.59c-.56 0-1.04.2-1.43.59-.39.39-.58.86-.58 1.41z"
-                        fill-rule="evenodd"
-                    ></path>
+            <CloseButton>
+                <svg width="25" height="25" viewBox="0 0 25 25" class="dw">
+                    <path d="M18.13 6.11l-5.61 5.61-5.6-5.61-.81.8 5.61 5.61-5.61 5.61.8.8 5.61-5.6 5.61 5.6.8-.8-5.6-5.6 5.6-5.62"></path>
                 </svg>
-            </DotButton>
+            </CloseButton>
         </ResponseWriterWrapper>
     );
 };
