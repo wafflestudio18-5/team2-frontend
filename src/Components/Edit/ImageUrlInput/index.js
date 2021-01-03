8c2afb6b315a837c1588855b3ebaa323e0f222e9
gnarmeister <sb9155118@naver.com>
2021-01-03 19:36:30 +0900

    Fix position of url input modal

--- src/Components/Edit/ImageUrlInput/index.js
+++ src/Components/Edit/ImageUrlInput/index.js
@@ -1,16 +1,18 @@
 import styled from "styled-components"
 
 const InputWrapper = styled.div`
-  position: absolute;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
   top: 0;
-  bottom: 0;
   left: 0;
-  right: 0;
   background: rgba(255, 255, 255, 0.8);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const InputContent = styled.div`
-  margin: auto;
   width: 800px;
   height: 600px;
   background: #fff;
