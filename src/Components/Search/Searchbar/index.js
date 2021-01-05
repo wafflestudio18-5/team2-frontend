369ccbfedc0d2fe89cf710809acfa1c3d61117e4
gnarmeister <sb9155118@naver.com>
2021-01-05 13:20:17 +0900

    Fix input style

--- src/Components/Search/Searchbar/index.js
+++ src/Components/Search/Searchbar/index.js
@@ -1,5 +1,20 @@
+import styled from "styled-components"
+
+const SearchbarStyle = styled.input`
+  margin-bottom: 40px;
+  border: 0;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  font-size: 49px;
+  font-family: "Noto Sans";
+  height: 80px;
+  width: 100%;
+  box-sizing: border-box;
+  outline: 0;
+`
+
 const Searchbar = () => {
-  return <div>searchbar</div>
+  let placeholder = "Search Wadium"
+  return <SearchbarStyle placeholder={placeholder} />
 }
 
 export default Searchbar
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -4,7 +4,29 @@ import Main from "./Main"
 import Searchbar from "./Searchbar"
 import Sidebar from "./Sidebar"
 
-const SearchStyle = styled.div``
+const SearchStyle = styled.div`
+  position: relative;
+  display: flex;
+`
+
+const SearchWrapper = styled.div`
+  margin-top: 65px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding-top: 40px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  box-sizing: border-box;
+  max-width: 1000px;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    margin-top: 56px;
+  }
+`
 
 const SearchResult = styled.div``
 
@@ -12,11 +34,13 @@ const Search = () => {
   return (
     <SearchStyle>
       <Header />
-      <Searchbar />
-      <SearchResult>
-        <Main />
-        <Sidebar />
-      </SearchResult>
+      <SearchWrapper>
+        <Searchbar />
+        <SearchResult>
+          <Main />
+          <Sidebar />
+        </SearchResult>
+      </SearchWrapper>
     </SearchStyle>
   )
 }
