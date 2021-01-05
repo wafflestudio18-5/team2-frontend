30d562578493e025c923dee959ef966af40da4bd
gnarmeister <sb9155118@naver.com>
2021-01-05 12:27:33 +0900

    Make basic structure of search page

---
+++ src/Components/Search/Searchbar/index.js
@@ -0,0 +1,5 @@
+const Searchbar = () => {
+  return <div>searchbar</div>
+}
+
+export default Searchbar
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,5 +1,24 @@
+import styled from "styled-components"
+import Header from "./Header"
+import Main from "./Main"
+import Searchbar from "./Searchbar"
+import Sidebar from "./Sidebar"
+
+const SearchStyle = styled.div``
+
+const SearchResult = styled.div``
+
 const Search = () => {
-  return <div>hi</div>
+  return (
+    <SearchStyle>
+      <Header />
+      <Searchbar />
+      <SearchResult>
+        <Main />
+        <Sidebar />
+      </SearchResult>
+    </SearchStyle>
+  )
 }
 
 export default Search
