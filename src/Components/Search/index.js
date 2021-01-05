12443207071ab75df78174c1bc5f5e5133639037
gnarmeister <sb9155118@naver.com>
2021-01-05 12:17:38 +0900

    Modify routing

---
+++ src/Components/Search/index.js
@@ -0,0 +1,5 @@
+const Search = () => {
+  return <div>hi</div>
+}
+
+export default Search
---
+++ src/Pages/Search/SearchPage.js
@@ -0,0 +1,7 @@
+import Search from "../../Components/Search"
+
+const SearchPage = () => {
+  return <Search />
+}
+
+export default SearchPage
