4fc8f053a1000c551c6c4d30f47294aa4dc48d78
jeongdaun <daun20211@naver.com>
2021-01-07 02:37:08 +0900

    Changed postRespond to promise

--- src/Pages/Story/Functions/postResponse.js
+++ src/Pages/Story/Functions/postResponse.js
@@ -1,16 +1,17 @@
 import { postComment } from '../../../api';
 
 const postResponse = async (token, response, storyid, setResponse, setResponseNum) => {
-    try {
-        const postrespond = await postComment(token, response, storyid);
-        setResponse(responses => {
-            console.log(...responses)
-            return [...responses, postrespond.data];
-        });
-        setResponseNum(num => {return num+1})
-    } catch (error) {
-        console.log(error);
-    }
+    postComment(token, response, storyid)
+        .then(postrespond => {
+            setResponse(responses => {
+                console.log(...responses);
+                return [...responses, postrespond.data];
+            });
+            setResponseNum(num => {
+                return num + 1;
+            });
+        })
+        .catch(error => console.log(error));
 };
 
 export default postResponse;
