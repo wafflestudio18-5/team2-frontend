0a634c4eadc7d4696ef69713f45d9e393fe6c164
gnarmeister <sb9155118@naver.com>
2021-01-06 14:17:41 +0900

    Pass googleLogin function to button

--- src/Container/AuthModal/Functions/googleLogin.js
+++ src/Container/AuthModal/Functions/googleLogin.js
@@ -1,17 +1,16 @@
 import { getAccountsGoogleLogin } from "../../../api"
 
-const googleLogin = async (history) => {
+const googleLogin = async () => {
   try {
-    // const response = await getAccountsGoogleLogin()
-    // const queryString = response.data.url.split("&")
-    // const redirect_uri = "https://www.wadium.shop/callback/google/"
-    // const url = [
-    //   queryString[0],
-    //   "redirect_uri=" + encodeURIComponent(redirect_uri),
-    //   ...queryString.slice(2),
-    // ].join("&")
-    // history.push(url)
-    history.push("https://naver.com/")
+    const response = await getAccountsGoogleLogin()
+    const queryString = response.data.url.split("&")
+    const redirect_uri = "https://www.wadium.shop/callback/google/"
+    const url = [
+      queryString[0],
+      "redirect_uri=" + encodeURIComponent(redirect_uri),
+      ...queryString.slice(2),
+    ].join("&")
+    window.location.href = url
   } catch (error) {
     console.log(error)
   }
