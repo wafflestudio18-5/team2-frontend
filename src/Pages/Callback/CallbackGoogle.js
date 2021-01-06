2b3acd77193830a696fd04a84ab8f1dde7c69f81
gnarmeister <sb9155118@naver.com>
2021-01-06 15:10:18 +0900

    Send social login request in callback/google/ page

--- src/Pages/Callback/CallbackGoogle.js
+++ src/Pages/Callback/CallbackGoogle.js
@@ -1,7 +1,18 @@
 import { useEffect } from "react"
+import { useLocation, useHistory } from "react-router-dom"
+import { useCookies } from "react-cookie"
 import Loading from "../../Components/Loading"
+import googleLogin from "./Functions/googleLogin"
 
 const CallbackGooglePage = () => {
+  const queryString = useLocation().search
+  const setCookie = useCookies(["auth"])[1]
+  const history = useHistory()
+
+  useEffect(() => {
+    googleLogin(queryString, setCookie, history)
+  }, [history, queryString, setCookie])
+
   return <Loading />
 }
 
