1208254b465d34c6cb8506ab396d6e4cf9fd2497
gnarmeister <sb9155118@naver.com>
2021-01-07 14:57:34 +0900

    Fix style

--- src/Components/MeStories/Main/index.js
+++ src/Components/MeStories/Main/index.js
@@ -5,11 +5,36 @@ import MainList from "./MainList"
 
 const MainWrapper = styled.div`
   margin-top: 129px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 64px;
+  padding-left: 64px;
+  padding-bottom: 64px;
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1320px;
   display: flex;
   flex-direction: column;
 
+  @media (max-width: 1080px) {
+    max-width: 1080px;
+  }
+
+  @media (max-width: 903.98px) {
+    max-width: 904px;
+    padding-right: 48px;
+    padding-left: 48px;
+  }
+
   @media (max-width: 767px) {
     margin-top: 80px;
+    padding-bottom: 24px;
+  }
+
+  @media (max-width: 552px) {
+    max-width: 552px;
+    padding-right: 24px;
+    padding-left: 24px;
   }
 `
 
