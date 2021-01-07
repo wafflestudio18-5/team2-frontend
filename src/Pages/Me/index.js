6b9b202bfae509eec1706e8c85cbdb7c96a39b35
gnarmeister <sb9155118@naver.com>
2021-01-07 12:21:43 +0900

    Redirect when url is invalid

--- src/Pages/Me/index.js
+++ src/Pages/Me/index.js
@@ -1,5 +1,5 @@
 import { routes } from "./Routes"
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route, Redirect } from "react-router-dom"
 
 const MePage = () => {
   return (
@@ -9,6 +9,7 @@ const MePage = () => {
         path={routes.stories.path}
         component={routes.stories.component}
       />
+      <Redirect to={routes.stories.path} />
     </Switch>
   )
 }
