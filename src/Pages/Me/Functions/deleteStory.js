0ddce886ed00ce71eb2f281c916e252370707c7b
gnarmeister <sb9155118@naver.com>
2021-01-07 17:26:22 +0900

    Reload after delete sstory

--- src/Pages/Me/Functions/deleteStory.js
+++ src/Pages/Me/Functions/deleteStory.js
@@ -3,6 +3,7 @@ import { deleteStoryStoryId } from "../../../api"
 const deleteStory = async (token, selectedStoryId) => {
   try {
     const response = await deleteStoryStoryId(token, selectedStoryId)
+    window.location.reload()
     return response
   } catch (error) {
     console.log(error)
