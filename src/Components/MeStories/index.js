4cec5ba76b84bf1f3df1f4cfba19a0de4b5c1915
gnarmeister <sb9155118@naver.com>
2021-01-07 13:30:19 +0900

    Add header

--- src/Components/MeStories/index.js
+++ src/Components/MeStories/index.js
@@ -1,5 +1,14 @@
-const MeStories = () => {
-  return <div>hi</div>
+import styled from "styled-components"
+import Header from "./Header"
+
+const MeStoriesStyle = styled.div``
+
+const MeStories = ({ user, openDropdown }) => {
+  return (
+    <MeStoriesStyle>
+      <Header user={user} openDropdown={openDropdown} />
+    </MeStoriesStyle>
+  )
 }
 
 export default MeStories
