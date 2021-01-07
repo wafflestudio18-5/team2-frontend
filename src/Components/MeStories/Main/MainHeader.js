123ab24c38d5b75fae8dca6776fdab7ac177f731
gnarmeister <sb9155118@naver.com>
2021-01-07 15:07:06 +0900

    Finish main header

--- src/Components/MeStories/Main/MainHeader.js
+++ src/Components/MeStories/Main/MainHeader.js
@@ -1,7 +1,43 @@
 import styled from "styled-components"
+import Color from "../../../Constants/Color"
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding-bottom: 40px;
+`
+
+const HeaderMessage = styled.h1`
+  line-height: 52px;
+  font-size: 42px;
+  font-family: "Noto Sans";
+  color: ${Color.borderBlack};
+  margin: 0;
+`
+
+const HeaderLink = styled.a`
+  text-decoration: none;
+  border: 1px solid ${Color.green};
+  color: ${Color.green};
+  padding: 9px 16px 7px;
+  font-family: "Noto Sans";
+  border-radius: 4px;
+
+  &:hover {
+    border-color: ${Color.hoverGreen};
+    color: ${Color.hoverGreen};
+  }
+`
 
 const MainHeader = () => {
-  return <div>header</div>
+  return (
+    <HeaderWrapper>
+      <HeaderMessage>Your stories</HeaderMessage>
+      <HeaderLink href="/edit">Write a story</HeaderLink>
+    </HeaderWrapper>
+  )
 }
 
 export default MainHeader
