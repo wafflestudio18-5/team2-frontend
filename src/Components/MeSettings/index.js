e56d1fce73822252e2e14333c15986c7e45e3021
gnarmeister <sb9155118@naver.com>
2021-01-08 17:44:02 +0900

    Add header in settings page

--- src/Components/MeSettings/index.js
+++ src/Components/MeSettings/index.js
@@ -1,7 +1,39 @@
 import styled from "styled-components"
+import Header from "./Header"
+import UserDropdown from "../MainLogin/UserDropdown"
 
-const MeSettings = () => {
-  return <div>hi</div>
+const SettingsStyle = styled.div``
+
+const MeSettings = ({
+  user,
+  isDropdownOpened,
+  openDropdown,
+  hideDropdown,
+  signOut,
+  isSearchboxOpen,
+  onClickSearchButton,
+  onChangeSearchbox,
+  search,
+}) => {
+  return (
+    <SettingsStyle>
+      <Header
+        user={user}
+        openDropdown={openDropdown}
+        isSearchboxOpen={isSearchboxOpen}
+        onClickSearchButton={onClickSearchButton}
+        onChangeSearchbox={onChangeSearchbox}
+        search={search}
+      />
+      {isDropdownOpened && (
+        <UserDropdown
+          user={user}
+          signOut={signOut}
+          hideDropdown={hideDropdown}
+        />
+      )}
+    </SettingsStyle>
+  )
 }
 
 export default MeSettings
--- src/Pages/Me/MeSettingsPage.js
+++ src/Pages/Me/MeSettingsPage.js
@@ -1,7 +1,50 @@
 import MeSettings from "../../Components/MeSettings"
+import { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { useCookies } from "react-cookie"
+import getCurrentUser from "../Main/Functions/getCurrentUser"
+import onClickSearchButton from "../Main/Functions/onClickSearchButton"
+import onChangeSearchbox from "../Main/Functions/onChangeSearchbox"
+import search from "../Main/Functions/search"
 
 const MeSettingsPage = () => {
-  return <MeSettings />
+  const [cookie, , removeCookie] = useCookies(["auth"])
+  const token = cookie.auth
+  const history = useHistory()
+
+  if (token === "" || token === undefined) {
+    // 로그인 하지 않았으면 main page로 redirect
+    history.push("/")
+  }
+
+  useEffect(() => {
+    getCurrentUser(token, setUser)
+  }, [token])
+
+  // states
+  // user
+  const [user, setUser] = useState({})
+  // 헤더의 검색창이 열려있는지 닫혀있는지
+  const [isSearchboxOpen, setIsSearchboxOpen] = useState(false)
+  // 검색창의 value
+  const [searchValue, setSearchValue] = useState("")
+  // Dropdown 표시 여부
+  const [isDropdownOpened, setIsDropdownOpened] = useState(false)
+
+  return (
+    <MeSettings
+      user={user}
+      isDropdownOpened={isDropdownOpened}
+      openDropdown={() => setIsDropdownOpened(true)}
+      hideDropdown={() => setIsDropdownOpened(false)}
+      isSearchboxOpen={isSearchboxOpen}
+      onClickSearchButton={() =>
+        onClickSearchButton(isSearchboxOpen, setIsSearchboxOpen, history)
+      }
+      onChangeSearchbox={(event) => onChangeSearchbox(event, setSearchValue)}
+      search={(event) => search(event, searchValue, history)}
+    />
+  )
 }
 
 export default MeSettingsPage
