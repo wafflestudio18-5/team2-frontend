55868c19a99dd7f9206932e5a5d23121aab567cc
gnarmeister <sb9155118@naver.com>
2021-01-08 18:34:10 +0900

    Fix style

--- src/Components/MeSettings/Main/index.js
+++ src/Components/MeSettings/Main/index.js
@@ -1,21 +1,69 @@
 import styled from "styled-components"
+import Color from "../../../Constants/Color"
 
 const Wrapper = styled.div`
   font-family: "Noto Sans";
-  margin-top: 100px;
+  margin-top: 129px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 64px;
+  padding-left: 64px;
+  padding-bottom: 64px;
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1320px;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+  max-width: 1032px;
+  color: ${Color.borderBlack};
 `
 
-const Block = styled.div``
+const Block = styled.div`
+  margin-bottom: 40px;
+`
 
-const InputTag = styled.h4``
+const InputTag = styled.h4`
+  font-size: 24px;
+  margin: 0;
+  margin-bottom: 10px;
+`
 
-const Input = styled.input``
+const Input = styled.input`
+  border: 0;
+  outline: 0;
+  width: 100%;
+  max-width: 500px;
+  border-bottom: 1px solid ${Color.lightGray};
+  font-size: 18px;
+  margin: 0;
+`
 
-const Message = styled.p``
+const Message = styled.p`
+  color: ${Color.gray};
+  font-size: 14px;
+  margin: 0;
+  margin-top: 4px;
+`
 
-const Button = styled.button``
+const Button = styled.button`
+  cursor: pointer;
+  background: none;
+  width: 60px;
+  height: 40px;
+  border: 1px solid ${Color.lightGray};
+  color: ${Color.gray};
+  border-radius: 4px;
 
-const Main = ({ userSpec, onChangeInput, save }) => {
+  &:hover {
+    border-color: ${Color.gray};
+    color: ${Color.borderBlack};
+  }
+`
+
+const ErrorMessage = styled.p``
+
+const Main = ({ userSpec, onChangeInput, save, errorMessage }) => {
   return (
     <Wrapper>
       <Block>
@@ -27,7 +75,9 @@ const Main = ({ userSpec, onChangeInput, save }) => {
         />
         <Message>
           Your name appears on your Profile page, as your byline, and in your
-          responses. It is a required field.
+          responses.
+          <br></br>
+          It is a required field.
         </Message>
       </Block>
       <Block>
@@ -50,19 +100,14 @@ const Main = ({ userSpec, onChangeInput, save }) => {
         />
         <Message>
           Your photo appears on your Profile page and with your stories across
-          Medium.
-          <br></br>
+          Wadium.
           <br></br>
           Recommended size: Square, at least 1000 pixels per side. File type:
           JPG, PNG or GIF.
         </Message>
       </Block>
       <Button onClick={save}>Save</Button>
-      <p>
-        {userSpec.name}
-        {userSpec.bio}
-        {userSpec.profile_image}
-      </p>
+      <ErrorMessage>{errorMessage}</ErrorMessage>
     </Wrapper>
   )
 }
