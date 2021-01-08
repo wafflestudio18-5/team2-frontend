18193a1deeaeef57ab0b3ca6b2bacf05882dd153
gnarmeister <sb9155118@naver.com>
2021-01-08 14:18:17 +0900

    Change error image

--- src/Pages/Error/index.js
+++ src/Pages/Error/index.js
@@ -1,10 +1,13 @@
 import styled from "styled-components"
-import error_image from "../../Images/error_image.png"
+import error_image from "../../Images/error_image.svg"
 
 const ErrorPageStyle = styled.img`
   width: 100vw;
   height: 100vh;
   object-fit: cover;
+  @media (max-width: 400px) {
+    height: 667px;
+  }
 `
 
 const ErrorPage = () => {
