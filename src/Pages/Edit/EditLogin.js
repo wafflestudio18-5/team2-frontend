3533db1f63dc2048d400a572744a082aaa396d16
gnarmeister <sb9155118@naver.com>
2021-01-08 13:27:21 +0900

    Redirect to main page if editting user and story wirter is different

--- src/Pages/Edit/EditLogin.js
+++ src/Pages/Edit/EditLogin.js
@@ -34,12 +34,14 @@ const EditLoginPage = ({ token }) => {
 
   useEffect(() => {
     getCurrentUser(token, setUser)
+  }, [token])
+
+  useEffect(() => {
     if (storyId !== "" && storyId !== undefined) {
-      console.log(token)
-      getStory(storyId, setStory, token)
+      getStory(storyId, setStory, token, user.id, history)
       id.current = storyId
     }
-  }, [token, storyId])
+  }, [token, storyId, user, history])
 
   // 글 저장 관련
   const [saveStatus, setSaveStatus] = useState(SaveStatusConstants.INIT)
--- src/Pages/Edit/Functions/getStory.js
+++ src/Pages/Edit/Functions/getStory.js
@@ -1,8 +1,12 @@
 import { getStoryById } from "../../../api"
 
-const getStory = async (id, setStory, token) => {
+const getStory = async (id, setStory, token, userId, history) => {
   try {
     const response = await getStoryById(id, token)
+    if (userId !== undefined && response.data.writer.id !== userId) {
+      history.push("/")
+      return
+    }
     setStory(response.data.body)
     return response
   } catch (error) {
