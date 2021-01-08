8cb9a544c5faef7042f9bd6ae5bc6639edd4866a
jeongdaun <daun20211@naver.com>
2021-01-08 22:20:30 +0900

    Set Setting for empty page

--- src/Components/User/Main/StoryList.js
+++ src/Components/User/Main/StoryList.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import StoryBlock from './StoryBlock'
+import StoryBlock from './StoryBlock';
 
 const StoryListStyle = styled.div`
     margin: 0 64px;
@@ -7,16 +7,37 @@ const StoryListStyle = styled.div`
     width: 100%;
 `;
 
+const EmptyText = styled.h4`
+    line-height: 24px;
+    font-size: 16px;
+    color: rgba(117, 117, 117, 1);
+    font-family: sohne, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+    font-weight: 400;
+    text-align: center;
+    min-height: 
+`;
+
+const WriteText = styled.a``;
 
 const StoryList = ({ UserStory, targetRef }) => {
-    return (
-        <StoryListStyle>
-            {UserStory.map((story, index) => {
-                return <StoryBlock story={story} />;
-            })}
-            <div ref={targetRef} />
-        </StoryListStyle>
-    );
+    if (UserStory.length !== 0)
+        return (
+            <StoryListStyle>
+                {UserStory.map((story, index) => {
+                    return <StoryBlock story={story} />;
+                })}
+                <div ref={targetRef} />
+            </StoryListStyle>
+        );
+    else
+        return (
+            <StoryListStyle>
+                <EmptyText>
+                    Have something to share?
+                    <WriteText href="/edit">Write your first story</WriteText>
+                </EmptyText>
+            </StoryListStyle>
+        );
 };
 
 export default StoryList;
