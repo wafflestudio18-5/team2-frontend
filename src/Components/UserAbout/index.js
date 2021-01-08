786d8ccddb119f49f703889a84403362922a6fcb
gnarmeister <sb9155118@naver.com>
2021-01-08 23:11:58 +0900

    Add header and dropdown to header

---
+++ src/Components/UserAbout/index.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+import Header from "../Story/Header"
+import UserDropdown from "../MainLogin/UserDropdown"
+
+const Wrapper = styled.div``
+
+const UserAbout = (
+  showModal,
+  userinfo,
+  logged_in,
+  me,
+  isSearchboxOpen,
+  onClickSearchButton,
+  onChangeSearchbox,
+  search,
+  openDropdown,
+  signOut,
+  isDropdownOpened,
+  hideDropdown
+) => {
+  return (
+    <Wrapper>
+      <Header
+        showModal={showModal}
+        userinfo={userinfo}
+        logged_in={logged_in}
+        me={me}
+        isSearchboxOpen={isSearchboxOpen}
+        onClickSearchButton={onClickSearchButton}
+        onChangeSearchbox={onChangeSearchbox}
+        search={search}
+        openDropdown={openDropdown}
+      />
+      {isDropdownOpened && (
+        <UserDropdown user={me} signOut={signOut} hideDropdown={hideDropdown} />
+      )}
+    </Wrapper>
+  )
+}
+
+export default UserAbout
