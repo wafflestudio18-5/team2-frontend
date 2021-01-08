5b9752cd65b44d243fc7754af93ccd3080b93c86
gnarmeister <sb9155118@naver.com>
2021-01-08 17:28:01 +0900

    Resolve warnings

--- src/Components/Story/Main/About/WriterInfo.js
+++ src/Components/Story/Main/About/WriterInfo.js
@@ -1,86 +1,66 @@
-import styled from 'styled-components';
-import Color from '../../../../Constants/Color';
+import styled from "styled-components"
+import Color from "../../../../Constants/Color"
 
 const WriterInfoBlock = styled.div`
-    display: block;
-    padding-bottom: 28px;
-    border-bottom: 1px solid ${Color.lightGray};
-`;
+  display: block;
+  padding-bottom: 28px;
+  border-bottom: 1px solid ${Color.lightGray};
+`
 
 const WrittenBy = styled.p`
-    text-transform: uppercase;
-    letter-spacing: 0.083em;
-    line-height: 16px;
-    font-size: 12px;
-    color: ${Color.gray};
-    font-family: sohne, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    font-weight: 400;
-    margin: 0;
-`;
+  text-transform: uppercase;
+  letter-spacing: 0.083em;
+  line-height: 16px;
+  font-size: 12px;
+  color: ${Color.gray};
+  font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-weight: 400;
+  margin: 0;
+`
 
 const UserNameBlock = styled.div`
-    display: block;
-    padding-top: 5px;
-    padding-bottom: 5px;
-`;
+  display: block;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`
 
 const UserName = styled.a`
-    font-weight: 500;
-    color: ${Color.black};
-    font-size: 16px;
-    word-break: break-word;
-    letter-spacing: 0;
-    line-height: 20px;
-    font-family: sohne, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    text-decoration: none;
-    cursor: pointer;
-`;
+  font-weight: 500;
+  color: ${Color.black};
+  font-size: 16px;
+  word-break: break-word;
+  letter-spacing: 0;
+  line-height: 20px;
+  font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+  text-decoration: none;
+  cursor: pointer;
+`
 
 const UserInfoBlock = styled.div`
-    display: block;
-    padding-top: 2px;
-`;
+  display: block;
+  padding-top: 2px;
+`
 
 const UserInfo = styled.p`
-    color: ${Color.gray};
-    line-height: 20px;
-    font-size: 14px;
-    font-family: sohne, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    margin: 0;
-`;
+  color: ${Color.gray};
+  line-height: 20px;
+  font-size: 14px;
+  font-family: sohne, "Helvetica Neue", Helvetica, Arial, sans-serif;
+  margin: 0;
+`
 
-const FollowBlock = styled.div`
-    display: block;
-    padding-top: 14px;
-`;
+const WriterInfo = ({ userinfo }) => {
+  return (
+    <WriterInfoBlock>
+      <WrittenBy>written by</WrittenBy>
+      <UserNameBlock>
+        <UserName href={userinfo.url}>{userinfo.name}</UserName>
+      </UserNameBlock>
+      <UserInfoBlock>
+        <UserInfo>{userinfo.userinfo}</UserInfo>
+      </UserInfoBlock>
+    </WriterInfoBlock>
+  )
+}
 
-const FollowButton = styled.button`
-    display: inline-block;
-    padding: 4px 12px 6px;
-    border-color: ${Color.gray};
-    color: ${Color.black};
-    text-decoration: none;
-    border-width: 1px;
-    border-radius: 4px;
-    background: 0;
-    line-height: 20px;
-    font-size: 14px;
-    font-family: sohne, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-    cursor: pointer;
-`;
-
-const WriterInfo = ({userinfo}) => {
-    return (
-        <WriterInfoBlock>
-            <WrittenBy>written by</WrittenBy>
-            <UserNameBlock>
-                <UserName href={userinfo.url}>{userinfo.name}</UserName>
-            </UserNameBlock>
-            <UserInfoBlock>
-                <UserInfo>{userinfo.userinfo}</UserInfo>
-            </UserInfoBlock>
-        </WriterInfoBlock>
-    );
-};
-
-export default WriterInfo;
+export default WriterInfo
